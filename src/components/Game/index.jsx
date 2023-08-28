be4d660544c9cb3614216fa5555e5847208b5049
Ana Molinari <121121296+anamolinari@users.noreply.github.com>
2023-08-28 19:37:11 -0300

    Update index.jsx

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./Game.module.css";
 import Player from "../Player";
 import {calculateWinner} from "../calculateWinner";
