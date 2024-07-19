b08ae39abffe9503632f84d61a501d1c4e3bfbed
Ana Molinari <121121296+anamolinari@users.noreply.github.com>
2024-07-19 17:43:00 +0200

    Update EndBoard - index.jsx

--- src/components/EndBoard/index.jsx
+++ src/components/EndBoard/index.jsx
@@ -1,47 +1,45 @@
 import React from "react";
-import  "./EndBoard.module.css"
+import "./EndBoard.module.css";
 
 function EndBoard({ winner }) {
-
-    if (winner === "X") {
-        return (
-            <div className="combined-endBoard">
-                <div className="image-endBoard">
-                    <img src="/X.svg" alt="X" />
-                </div>
-                <div className="text-endBoard">
-                    <h1>Winner</h1>
-                </div>
-            </div>
-        );
-    } else if (winner === "O") {
-        return (
-            <div className="combined-endBoard">
-                <div className="image-endBoard">
-                    <img src="/O.svg" alt="O" />
-                </div>
-                <div className="text-endBoard">
-                    <h1>Winner</h1>
-                </div>
-            </div>
-        );
-    } else if (winner === "Draw") {
-        return (
-            <div className="combined-endBoard">
-                <div className="image-endBoard">
-                    <img src="/X.svg" alt="X" />
-                    <img src="/O.svg" alt="O" />
-                </div>
-                <div className="text-endBoard">
-                    <h1>Draw</h1>
-                </div>
-            </div>
-        );
-        } else {
-            return null;
-        }
+  if (winner === "X") {
+    return (
+      <div className="combined-endBoard">
+        <div className="image-endBoard">
+          <img src="/X.svg" alt="X" />
+        </div>
+        <div className="text-endBoard">
+          <h1>Winner!</h1>
+        </div>
+      </div>
+    );
+  } else if (winner === "O") {
+    return (
+      <div className="combined-endBoard">
+        <div className="image-endBoard">
+          <img src="/O.svg" alt="O" />
+          os doi
+        </div>
+        <div className="text-endBoard">
+          <h1>Winner!</h1>
+        </div>
+      </div>
+    );
+  } else if (winner === "Draw") {
+    return (
+      <div className="combined-endBoard">
+        <div className="image-endBoard">
+          <img src="/X.svg" alt="X" />
+          <img src="/O.svg" alt="O" />
+        </div>
+        <div className="text-endBoard">
+          <h1>It was a draw!</h1>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default EndBoard;
-
-
